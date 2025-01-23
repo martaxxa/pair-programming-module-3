c8515ed8195664dd4bc6f812e062bbc3d1a25865
Celia Recio <celia.recio@yahoo.es>
2025-01-23 20:40:05 +0100

    build game infrastructure

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,9 +2,46 @@ import './styles/App.scss'
 
 function App() {
   return (
-    <div>
-        Escribe aquí tu HTML
-      </div>
+    <body class="page">
+    <header>
+      <h1>¡Cuidado con Grogu!</h1>
+    </header>
+    <main class="page">
+      <section class="board">
+        <div class="cell"><div class="grogu">👣</div></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+        <div class="cell"></div>
+      </section>
+
+      <section>
+        <button class="dice">Lanzar Dado</button>
+        <div class="game-status">En curso</div>
+      </section>
+
+      <section class="goods-container">
+        <div class="goods-item">🍪</div>
+        <div class="goods-item">🍪</div>
+        <div class="goods-item">🍪</div>
+      </section>
+      <section class="goods-container">
+        <div class="goods-item">🥚</div>
+        <div class="goods-item">🥚</div>
+        <div class="goods-item">🥚</div>
+      </section>
+      <section class="goods-container">
+        <div class="goods-item">🐸</div>
+        <div class="goods-item">🐸</div>
+        <div class="goods-item">🐸</div>
+      </section>
+      <section>
+        <button class="restart-button">Reiniciar Juego</button>
+      </section>
+    </main>
+  </body>
   )
 }
 

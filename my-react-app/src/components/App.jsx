06f5699fc744ffda282963407f9e2b9f9431c66c
Celia Recio <celia.recio@yahoo.es>
2025-01-28 21:48:48 +0100

    set text for results

--- my-react-app/src/components/App.jsx
+++ my-react-app/src/components/App.jsx
@@ -9,16 +9,26 @@ import Footer from './Footer';
 function App() {
   //selectores de contenido 
     const [dice, setDice] = useState(); 
+    const [text, setText] = useState();
   //funciones de interacción
-    console.log(dice); 
+    // console.log(dice); 
 
     const handleInputDice = (ev) => {
       const numeroAleatorio = Math.floor(Math.random() * 4) + 1; 
       setDice(numeroAleatorio);
-      if ( dice === 1 &&2 &&3) {
+      if ( numeroAleatorio === 1 ) {
+        setText (`el número es ${numeroAleatorio}: has descargado una galleta`);
+      } else if ( numeroAleatorio === 2 ) {
+        setText (`el número es ${numeroAleatorio}: has descargado un huevo`);
+      } else if ( numeroAleatorio === 3) { 
+        setText (`el número es ${numeroAleatorio}: has descargado una rana`);   
+      } else {
+        setText (`el número es ${numeroAleatorio}: Grogu avanza`);
+      };       
+    };
 
-      }
-    }
+    
+      
   return (
     <div className="page">
       <Header></Header>
@@ -35,8 +45,9 @@ function App() {
 
         <section>
           <button className="dice" onClick={handleInputDice}>Lanzar Dado</button>
-          <div className="game-status">En curso</div>
+          {/* <div className="game-status">{text}</div> */}
         </section>
+        <div className="game-status">{text}</div>
 
         <section className="goods-container">
           <div className="goods-item">🍪</div>
